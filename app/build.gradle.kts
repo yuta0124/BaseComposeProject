28024fc5bb9968f2a4ef0834ba21209610a858dc
satoyu <yu.sato@fenrir.co.jp>
2024-10-03 22:05:17 +0900

    build-logic追加

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,21 +1,25 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
+    id("baseComposeProject.primitive.androidapplication")
+    id("baseComposeProject.primitive.android.kotlin")
+    id("baseComposeProject.primitive.android.compose")
+    id("baseComposeProject.primitive.android.hilt")
+    id("baseComposeProject.primitive.detekt")
+    // TODO: 本来以下で良いはず
+//    id("baseComposeProject.convention.androidfeature")
 }
 
 android {
     namespace = "com.example.basecomposeproject"
-    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.basecomposeproject"
-        minSdk = 24
-        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
     }
 
     buildTypes {
@@ -27,33 +31,10 @@ android {
             )
         }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-    buildFeatures {
-        compose = true
-    }
-}
 
-dependencies {
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
 }
