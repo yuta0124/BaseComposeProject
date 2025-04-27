503b0135bb83c842fad5fe2479e59d437aae224e
satoyu <yu.sato@fenrir.co.jp>
2025-04-27 13:29:50 +0900

    ビルドが失敗する原因の解消

--- core/data/src/main/java/com/example/data/database/FavoritePokemonDatabase.kt
+++ core/data/src/main/java/com/example/data/database/FavoritePokemonDatabase.kt
@@ -3,7 +3,7 @@ package com.example.data.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [PokemonTable::class], version = 1)
+@Database(entities = [PokemonTable::class], version = 1, exportSchema = false)
 abstract class FavoritePokemonDatabase : RoomDatabase() {
     abstract fun pokemonDao(): PokemonDao
 }
